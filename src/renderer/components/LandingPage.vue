294909205a16b0e3b1fe90a839c9dee87b8c53f5
Your Name <you@example.com>
2017-10-10 08:37:44 +0300

    first commit

---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,19 @@
+<template>
+<div>Hello World</div>
+</template>
+
+<script>
+  import SystemInformation from './LandingPage/SystemInformation'
+
+  export default {
+    name: 'landing-page',
+    components: { SystemInformation },
+    methods: {
+
+    }
+  }
+</script>
+
+<style>
+
+</style>
