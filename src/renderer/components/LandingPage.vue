9d04486473c2f763c508403c006aab64399acec7
Your Name <you@example.com>
2017-10-10 08:44:39 +0300

    Hel

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -1,5 +1,19 @@
 <template>
-<div>Hello World</div>
+<table>
+  <tr>
+    <th>Month</th>
+    <th>Savings</th>
+  </tr>
+  <tr>
+    <td>January</td>
+    <td>$100</td>
+  </tr>
+  <tr>
+    <td>February</td>
+    <td>$80</td>
+  </tr>
+</table>
+
 </template>
 
 <script>
@@ -15,5 +29,7 @@
 </script>
 
 <style>
-
+table, th, td {
+    border: 1px solid black;
+}
 </style>
